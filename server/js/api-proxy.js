a4b97a116411f1dd7452e97e23b0a4df13b13742
Austin Davis <austindanieldavis@gmail.com>
2021-04-01 17:04:04 -0600

    add missing package

--- server/js/api-proxy.js
+++ server/js/api-proxy.js
@@ -2,17 +2,17 @@
 exports.__esModule = true;
 var express = require("express");
 var cors = require("cors");
+var http_proxy_middleware_1 = require("http-proxy-middleware");
 var PORT = 8082;
 var HOST = "localhost";
 var API_BASE_URL = "https://dog.ceo/api";
 var App = express();
 App.use(cors());
 App.options("*", cors());
-App.get("*", function (req, res) {
-    return req.body.json().then(function (value) {
-        res.send(value);
-    });
-});
+App.use("/", http_proxy_middleware_1.createProxyMiddleware({
+    target: API_BASE_URL,
+    changeOrigin: true
+}));
 App.listen(PORT, HOST, function () {
     console.log("Running API proxy server on port " + PORT);
 });
